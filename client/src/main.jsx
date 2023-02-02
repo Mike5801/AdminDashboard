1d25d914ca43d4842cad9e4013944a933051bfb7
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-02-01 19:23:40 -0600

    add redux configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import globalReducer from "state";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
