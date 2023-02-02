b4dec6ae14cb9cb107af1233a216eb8ccca2dae9
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-02-01 19:27:48 -0600

    add base style settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import createTheme from "@mui/material/styles";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
 
 function App() {
 
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
-  )
-}
+  );
+};
 
 export default App
