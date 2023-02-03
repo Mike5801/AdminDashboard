d202bc54cedba98575493244d5976dec18a14e99
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-02-02 22:45:58 -0600

    add redux query setup and profile for navbar and sidebar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,17 @@ import './index.css'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import globalReducer from "state";
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from "state/api";
 
 const store = configureStore({
   reducer: {
-    global: globalReducer
-  }
-})
+    global: globalReducer,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware)
+});
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import BodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -11,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+/* DATA IMPORTS */
+// import User from "./models/User.js";
+// import { dataUser } from "./data/index.js"
+
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
@@ -20,7 +23,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors);
+app.use(cors());
 
 /* ROUTES */
 app.use("/client", clientRoutes);
@@ -28,14 +31,20 @@ app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
+app.use("/", (req, res, next) => {
+    res.json({ message: "hola" })
+});
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // User.insertMany(dataUser)
 }).catch((error) => {
     console.log(`${error} did not connect`);
 });
